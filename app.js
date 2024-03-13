708f27101d17fada2c50d101a2a11c54e06c1eaa
KYHH <kyhh93@naver.com>
2024-03-13 14:24:44 +0900

    🔨

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.navbtn-all').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.navbtn-all').addEventListener('click', () => {
     showAllProducts();
   });
 
-  document.querySelector('.navbtn-501').addEventListener('click', function () {
+  document.querySelector('.navbtn-501').addEventListener('click', () => {
     showProductsByClass('sell-501');
   });
 
-  document.querySelector('.navbtn-502').addEventListener('click', function () {
+  document.querySelector('.navbtn-502').addEventListener('click', () => {
     showProductsByClass('sell-502');
   });
 
-  document.querySelector('.navbtn-505').addEventListener('click', function () {
+  document.querySelector('.navbtn-505').addEventListener('click', () => {
     showProductsByClass('sell-505');
   });
 
-  document.querySelector('.navbtn-LVC').addEventListener('click', function () {
+  document.querySelector('.navbtn-LVC').addEventListener('click', () => {
     showProductsByClass('sell-LVC');
   });
 });
 
-function showAllProducts() {
-  document.querySelectorAll('.sell').forEach(function (product) {
+const showAllProducts = () => {
+  document.querySelectorAll('.sell').forEach((product) => {
     product.style.display = 'block';
   });
 }
 
-function showProductsByClass(className) {
-  document.querySelectorAll('.sell').forEach(function (product) {
+const showProductsByClass = (className) => {
+  document.querySelectorAll('.sell').forEach((product) => {
     if (product.classList.contains(className)) {
       product.style.display = 'block';
-    } else {
+    }
+    if (!product.classList.contains(className)) {
       product.style.display = 'none';
     }
   });
